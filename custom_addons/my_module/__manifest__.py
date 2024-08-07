80ac3f2812176eb3c98f5bfe22959229f9c5d095
Pavlo <pavlo-admin-1c@ip-10-130-12-72.eu-west-1.compute.internal>
2024-08-07 13:58:46 +0300

    Added initial module structure

---
+++ custom_addons/my_module/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'My Module',
+    'version': '1.0',
+    'summary': 'My first custom module',
+    'description': 'This is a test module for Odoo development.',
+    'author': 'Your Name',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
+
